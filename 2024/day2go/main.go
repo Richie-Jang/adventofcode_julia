7b1e7e312c03ef364100b3a3db8c3507a92e7718
Richie Jang <richiejang@naver.com>
2025-12-07 14:05:43 +0900

    solved part2/day2/2024

--- 2024/day2go/main.go
+++ 2024/day2go/main.go
@@ -32,7 +32,168 @@ func check_line(vs []int) bool {
 	})
 }
 
+func check_line_part2_ex(vs []int) bool {
+
+	isvalid := func(arr []int) bool {
+		diff := 0
+		prevdiff := 0
+		for i := range arr {
+			pv := arr[i]
+			nv := arr[i+1]
+			diff = nv - pv
+			if diff == 0 {
+				return false
+			}
+
+			if diff > 3 || diff < -3 {
+				return false
+			}
+			if i == 0 {
+				prevdiff = diff
+				continue
+			}
+			if prevdiff*diff < 0 {
+				return false
+			}
+			prevdiff = diff
+			if i == len(arr)-2 {
+				break
+			}
+		}
+		return true
+	}
+
+	// spread condition vs
+	allvs := [][]int{}
+	allvs = append(allvs, vs)
+	for i := range vs {
+		nvs := []int{}
+		for j := 0; j < len(vs); j++ {
+			if i == j {
+				continue
+			}
+			nvs = append(nvs, vs[j])
+		}
+		allvs = append(allvs, nvs)
+	}
+	for _, arr := range allvs {
+		if isvalid(arr) {
+			return true
+		}
+	}
+	return false
+}
+
+// real hard coding...
+func check_line_part2(vs []int) bool {
+	checkers := []int{}
+	for i := range vs {
+		diff := vs[i+1] - vs[i]
+		checkers = append(checkers, diff)
+		if i == len(vs)-2 {
+			break
+		}
+	}
+
+	fmt.Println("start check:", vs, "->", checkers)
+
+	isvalid := func(arr []int) bool {
+		fmt.Println("final check arr: ", arr)
+		diff := 0
+		prevdiff := 0
+		for i := range arr {
+			pv := arr[i]
+			nv := arr[i+1]
+			diff = nv - pv
+			if diff == 0 {
+				return false
+			}
+			if diff > 3 || diff < -3 {
+				return false
+			}
+			if i == 0 {
+				prevdiff = diff
+				continue
+			}
+			if prevdiff*diff < 0 {
+				return false
+			}
+			prevdiff = diff
+			if i == len(arr)-2 {
+				break
+			}
+		}
+		return true
+	}
+
+	// remove same numbers
+	newvs := make([]int, len(vs))
+	copy(newvs, vs)
+
+	posval, negval := 0, 0
+	for _, v := range checkers {
+		if v > 0 {
+			posval++
+		} else if v < 0 {
+			negval++
+		}
+	}
+
+	fmt.Println("posval:", posval, "negval:", negval, "len:", len(checkers))
+
+	if posval+negval < len(checkers) {
+		zeroCount := len(checkers) - (posval + negval)
+		fmt.Println("zero found. ", zeroCount)
+		if zeroCount > 1 {
+			return false
+		} else if zeroCount == 1 {
+			_, pos, _ := lo.FindIndexOf(checkers, func(item int) bool {
+				return item == 0
+			})
+			newvs = append(vs[:pos], vs[pos+1:]...)
+			return isvalid(newvs)
+		}
+	} else {
+		if posval > negval && negval == 1 {
+			fmt.Println("remove neg 1 value")
+			_, pos, _ := lo.FindIndexOf(checkers, func(item int) bool {
+				return item < 0
+			})
+			newvs = append(vs[:pos+1], vs[pos+2:]...)
+			return isvalid(newvs)
+		} else if negval > posval && posval == 1 {
+			fmt.Println("remove pos 1 value")
+			_, pos, _ := lo.FindIndexOf(checkers, func(item int) bool {
+				return item > 0
+			})
+			newvs = append(vs[:pos+1], vs[pos+2:]...)
+			return isvalid(newvs)
+		}
+
+		// up down check
+		check4over := lo.CountBy(checkers, func(v int) bool {
+			return v >= 4 || v <= -4
+		})
+		if check4over > 1 {
+			fmt.Println("found over abs(4) value 2 more")
+			return false
+		} else if check4over == 1 {
+			_, pos, _ := lo.FindIndexOf(checkers, func(item int) bool {
+				return item >= 4 || item <= -4
+			})
+			fmt.Println("remove abs over 4 value at pos:", pos+1)
+			newvs = append(vs[:pos+1], vs[pos+2:]...)
+			return isvalid(newvs)
+		}
+
+	}
+
+	return isvalid(newvs)
+
+}
+
 // part1 solution
+// part2 solution
 func main() {
 	testcase := false
 	if len(os.Args) > 1 {
@@ -47,7 +208,13 @@ func main() {
 	defer r.Close()
 	br := bufio.NewScanner(r)
 
-	res := 0
+	part1 := 0
+	part2 := 0
+
+	/*
+		XNOR := func(a, b bool) bool {
+			return (a && b) || (!a && !b)
+		}*/
 
 	for br.Scan() {
 		t := strings.TrimSpace(br.Text())
@@ -56,11 +223,17 @@ func main() {
 			return v
 		})
 
-		if check_line(vs) {
-			fmt.Println(vs, " true")
-			res++
+		part1res := check_line(vs)
+		part2res := check_line_part2_ex(vs)
+		if part1res {
+			part1++
+		}
+
+		if part2res {
+			part2++
 		}
 	}
 
-	fmt.Println(res)
+	fmt.Println("part1: ", part1)
+	fmt.Println("part2: ", part2)
 }
